ce7400bee865d81d679fa736f83a1a0d6b68bdfb
Divya Chakshu Bisht <65598930+DCBisht@users.noreply.github.com>
2023-06-23 08:51:43 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Merge Sort/merge-sort.java
@@ -0,0 +1,112 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Merge_Sort
+{
+    //method to print the elements of the array
+	static void printArray(int arr[])
+    {
+        StringBuffer sb=new StringBuffer("");
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            sb.append(arr[i]+" ");
+        System.out.println(sb.toString());
+    }
+
+    
+
+	public static void main(String args[])
+	{
+	    //taking input using Scanner class
+		Scanner sc = new Scanner(System.in);
+		
+		//taking testcases
+		int T = sc.nextInt();
+		while(T>0)
+		{
+		    //taking elements count
+			int n = sc.nextInt();
+			
+			//creating an object of class Merge_Sort
+			Merge_Sort ms = new Merge_Sort();
+			
+			//creating an array of size n
+			int arr[] = new int[n];
+			
+			//adding elements to the array
+			for(int i=0;i<n;i++)
+				arr[i] = sc.nextInt();
+
+            
+			Solution g = new Solution();
+			
+			//calling the method mergeSort
+			g.mergeSort(arr,0,arr.length-1);
+
+            //calling the method printArray
+			ms.printArray(arr);
+		T--;
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    void merge(int arr[], int l, int m, int r)
+    {
+        //   Your code here
+         int n1=m-l+1;
+         int n2=r-m;
+         int a1[]=new int[n1];
+         int a2[]=new int[n2];
+         int x=0;
+         for(int i=0;i<n1;i++){
+             a1[i]=arr[l+i];
+         }
+         x=0;
+         for(int i=0;i<n2;i++){
+             a2[i]=arr[m+1+i];
+         }
+        //  System.out.println(Arrays.toString(a1));
+        //  System.out.println(Arrays.toString(a2));
+         int y=0,i=l;
+         x=0;
+         while(x<n1 && y<n2){
+             if(a1[x]<a2[y]){
+                 arr[i]=a1[x];
+                 x++;
+             }
+             else{
+                 arr[i]=a2[y];
+                 y++;
+             }
+             i++;
+             
+         }
+         while(x<n1){
+             arr[i++]=a1[x++];
+         }
+         
+         while(y<n2){
+             arr[i++]=a2[y++];
+         }
+         
+    }
+    void mergeSort(int arr[], int l, int r)
+    {
+        //code here
+        if(l>=r){
+            return;
+        }
+        int mid = l+(r-l)/2;
+        mergeSort(arr,l,mid);
+        mergeSort(arr,mid+1,r);
+        merge(arr,l,mid,r);
+        
+    }
+}
